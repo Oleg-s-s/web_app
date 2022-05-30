26267b964c595ab5f9e39cc188b35017034b2747
Oleg-s-s <salitrunskyy@gmail.com>
2022-05-30 19:21:06 +0300

    3

--- web_test/main/views.py
+++ web_test/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>qwerty</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>wasd</h4>")
+    return render(request, 'main/about.html')
