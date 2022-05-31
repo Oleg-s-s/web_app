f6690dadfdb3df40720e2b2e46ebf9a9b26c75fd
Oleg-s-s <salitrunskyy@gmail.com>
2022-05-31 19:07:18 +0300

    4

--- web_test/main/views.py
+++ web_test/main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Main Page',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMW',
+            'age': 20,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
--- web_test/web_test/settings.py
+++ web_test/web_test/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my apps
     'main',
+    'news',
 
     # django apps
     'django.contrib.admin',
@@ -121,9 +122,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
