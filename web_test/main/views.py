fb85633b3375a8289b8d592574ba54795a2dc233
Oleg-s-s <salitrunskyy@gmail.com>
2022-05-31 20:03:14 +0300

    5

--- web_test/main/views.py
+++ web_test/main/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import render
 def index(request):
     data = {
         'title': 'Main Page',
-        'values': ['Some', 'Hello', '123'],
-        'obj': {
-            'car': 'BMW',
-            'age': 20,
-            'hobby': 'Football'
-        }
     }
     return render(request, 'main/index.html', data)
 
