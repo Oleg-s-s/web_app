01c7a3d50d8b97c75bcbbd24b78280792ee5aa19
Oleg-s-s <salitrunskyy@gmail.com>
2022-06-02 17:44:30 +0300

    6

--- web_test/news/views.py
+++ web_test/news/views.py
@@ -4,3 +4,6 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news':news})
+
+def create_news(request):
+    return render(request, 'news/create.html')
